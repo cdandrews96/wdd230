eab25aee98f1ada1e40a158fe103c023074e7138
cdandrews96 <and16137@byui.edu>
2024-11-16 00:36:21 -0700

    finished creating cards

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,7 +4,34 @@ const cards = document.querySelector('#cards');
 async function getMemberData() {
     const response = await fetch(membersUrl);
     const data = await response.json();
+    // console.table(data.members);
     displayMembers(data.members);
 }
 
 getMemberData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('a');
+
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `Company logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '60%');
+        logo.setAttribute('height', '100%');
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phone}`;
+        url.setAttribute('href', `${member.urls}`);
+
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        cards.appendChild(card);
+    });
+}
