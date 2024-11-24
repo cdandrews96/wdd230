4ceb828adaa296c5b9261a5f411976d3c2a617e0
cdandrews96 <and16137@byui.edu>
2024-11-23 18:14:09 -0700

    weather forecast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -3,6 +3,7 @@ const weatherIcon = document.querySelector('#weathIcon');
 const temperature = document.querySelector('#temp');
 const weatherDesc = document.querySelector('#description');
 
+// Call Weather Info
 async function apiFetch() {
     try {
         const response = await fetch(url);
@@ -18,6 +19,7 @@ async function apiFetch() {
     }
 }
 
+
 async function displayResults () {
     const data = await apiFetch();
     // Get icon
@@ -28,3 +30,38 @@ async function displayResults () {
     temperature.innerHTML = `${data.main.temp} &deg;F`;
     weatherDesc.innerHTML = `${data.weather[0].description}`;
 }
+
+displayResults();
+
+//Forecast
+const forecasturl = `https://api.openweathermap.org/data/2.5/forecast?lat=40.76&lon=-111.88&appid=3aa3bb156d506476387dc30b596a71b9&units=imperial`
+const iconDay1 = document.querySelector('#day1');
+const weathDayOne = document.querySelector('#tempDayOne');
+const weathDayTwo = document.querySelector('#tempDayTwo');
+const weathDayThree = document.querySelector('#tempDayThree');
+
+// Call Forecast Info
+async function apiFetchForecast() {
+    try {
+        const response = await fetch(forecasturl);
+        if (response.ok) {
+            const forecastData = await response.json();
+            console.log(forecastData);
+            return forecastData;
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function displayForecastResults () {
+    const forecastData = await apiFetchForecast();
+    // Get icon
+    weathDayOne.innerHTML = `<strong>Tomorrow:</strong> ${forecastData.list[2].main.temp} &deg;F`;
+    weathDayTwo.innerHTML = `<strong>Next day:</strong> ${forecastData.list[10].main.temp} &deg;F`;
+    weathDayThree.innerHTML = `<strong>Day after:</strong> ${forecastData.list[18].main.temp} &deg;F`;
+}
+
+displayForecastResults();
